a8add4ddaa87e0edcd7710818c57ff41ccf8ad4c
Slavutich1024 <slavutich15@icloud.com>
2022-10-06 21:29:20 +0200

    RemovDupFromSortedList

---
+++ RemoveDupFromSortList/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+
+
+namespace RemoveDupFromSortList {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode DeleteDuplicates(ListNode head) {
+            if(head == null)
+                return head;
+
+            ListNode current = head;
+
+            while(current.next != null) {
+                if(current.val == current.next.val) 
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            }
+            return head;
+        }
+                
+        class Programm {
+            static void Main(string[] args) {
+               
+
+
+
+
+
+            }
+        }
+    }
+}
+                   
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
